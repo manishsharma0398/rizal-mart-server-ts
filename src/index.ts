2bc84a5a4da18b8f2e152c1c345f32c6c17e7210
Manish Sharma <manish.sharma0398@gmail.com>
2024-01-21 16:00:01 +0530

    feat: express and node type setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('test');
+import express from 'express';
+
+import authRoutes from '@routes/auth';
+
+// initialize express
+const app = express();
+
+// middlewares
+app.use(express.json());
+
+// routes
+app.use('/auth', authRoutes);
+
+app.get('/health', (req, res) => {
+  return res.json({ msg: 'Server running successfully' });
+});
+
+app.listen(3000, () => {
+  console.log('Server running on 3000');
+});
